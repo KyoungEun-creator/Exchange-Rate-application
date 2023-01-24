06f42105fb6d41f2dd7c9450de62c9cd4434bf13
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-24 17:56:47 +0100

    Implement two boxes for input and output
    1. two boxes
    2. drop-down list for each boxes

---
+++ main.js
@@ -0,0 +1,9 @@
+// two boxes
+// drop-down list for each boxes
+// bring currency ratio information
+// make it change depend on the item from drop-down list
+// show output depend on the input number
+// show output depend on the item from drop-down list
+// show easy way to read number in Korean
+// make it doesn't depend on the input-output box
+
