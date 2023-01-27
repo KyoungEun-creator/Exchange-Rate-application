a2147c37f3facb05f329b75c4434b0fe8a8fb120
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-27 16:00:16 +0100

    bring currency ratio information
    put currencyRatio infos as an Object type
    on it's way adding click event on drop down list

--- main.js
+++ main.js
@@ -7,3 +7,40 @@
 // show easy way to read number in Korean
 // make it doesn't depend on the input-output box
 
+let currencyRatio = {           //객체타입으로 정의
+    KRW:{
+        KRW:1,
+        EUR:0.00075,
+        USD:0.00081,
+        unit: "원"
+    },
+    EUR:{
+        KRW:1341.09,
+        EUR:1,
+        USD:1.09,
+        unit: "유로"
+    },
+    USD:{
+        KRW:1234.26,
+        EUR:0.92,
+        USD:1,
+        unit: "달러"
+    }
+}
+
+// console.log(currencyRatio.KRW.unit);
+// console.log(currencyRatio['EUR']['unit']);
+
+// if (document.querySelector('.dropdown-content').display=='block') {
+//     document.querySelector('.krw').addEventListener('click', function(){
+//         document.querySelector('.dropbtn').innerHTML = 'KRW'
+//     });
+// }
+
+let fromCurrency="Dropdown"
+
+document.querySelectorAll('#from-currency-list a').forEach(
+    (menu)=>menu.addEventListener('click', function(){
+    document.getElementById('from-button').textContent = this.textContent;
+    fromCurrency = this.textContent;
+}));
